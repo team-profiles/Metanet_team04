bf1b0d8d9ea34ac4a7068a5f86c476d1cf9d3d18
taehyung0519 <64758887+taehyung0519@users.noreply.github.com>
2023-11-07 22:09:53 +0900

    Update 11.07.181844.java

--- Array/11.07.java
+++ Array/11.07.java
@@ -1,6 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public int[] solution(int[] arr, int[] delete_list) {
-        int[] answer = {};
-        return answer;
+        List<Integer> resultList = new ArrayList<>();
+
+        for (int num : arr) {
+            boolean toDelete = false;
+
+            for (int deleteNum : delete_list) {
+                if (num == deleteNum) {
+                    toDelete = true;
+                    break;
+                }
+            }
+
+            if (!toDelete) {
+                resultList.add(num);
+            }
+        }
+
+        // ArrayList를 배열로 변환
+        int[] resultArray = new int[resultList.size()];
+        for (int i = 0; i < resultList.size(); i++) {
+            resultArray[i] = resultList.get(i);
+        }
+
+        return resultArray;
     }
 }
