eb0d50eefdd19225e9f39554d00dbac8c9a18dbd
lala9663 <105348713+lala9663@users.noreply.github.com>
2023-11-19 16:45:04 +0900

    231117 금 1번문제

---
+++ Array/태윤/11100655036.java
@@ -0,0 +1,21 @@
+    class Solution {
+        public int climbStairs(int n) {
+
+            if (n<=3){
+                return n;
+            }
+            int[] arr = new int[n];
+
+            arr[0] = 1;
+            arr[1] = 2;
+            arr[2] = 3;
+
+
+
+            for (int i = 3; i <n; i++) {
+                arr[i] = arr[i-1] + arr[i-2];
+            }
+
+            return arr[n-1];
+        }
+    }
