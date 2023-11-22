697d25b4988d2ed64072ec8036bd2d43c32f8e10
SUHYUN JI <68486112+hissue@users.noreply.github.com>
2023-11-22 21:33:48 +0900

    #44 [PG] 20231122 / 주사위 게임2 / 181930

---
+++ Array/수현/181930.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int a, int b, int c) {
+        int answer = 0;
+        int sum1 = a+b+c;
+        int sum2 = a*a + b*b + c*c;
+        int sum3 = a*a*a + b*b*b + c*c*c;
+        if(a==b && b==c){
+            return sum1*sum2*sum3;
+        }else if(a!=b && b!=c && a!=c){
+            return sum1;
+        }else
+            return sum1*sum2;
+    }
+}
